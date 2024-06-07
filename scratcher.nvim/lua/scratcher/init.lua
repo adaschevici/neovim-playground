fe5d6c5e36dc94da2e4cc9b64329bc7f0dcaea8a
Artur Das <a.daschevici@gmail.com>
2024-06-07 14:39:51 +0200

    Add a buffer creating function

--- scratcher.nvim/lua/scratcher/init.lua
+++ scratcher.nvim/lua/scratcher/init.lua
@@ -4,6 +4,13 @@ local function main()
   print("Hello from our plugin")
 end
 
+local function create_buffer()
+  local buf = vim.api.nvim_create_buf(true, true)
+  vim.api.nvim_buf_set_name(buf, "*scratch*")
+  vim.api.nvim_set_option_value("filetype", "lua", { buf = buf })
+  return buf
+end
+
 local function setup()
   vim.api.nvim_create_autocmd("VimEnter",
     { group = augroup, desc = "Set a scratch buffer on load", once = true, callback = main })
